4db99ca61cfa153512caab8faef7a397f3cb4ce4
karkar <waizinaung.mm142@gamil.com>
2023-09-22 16:06:26 +0630

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test2.Program.CallTest2();
+            Console.WriteLine("****Check Vowel***");
+            Console.WriteLine("Enter char:");
+            xyz: string a= Console.ReadLine().ToLower();
+            char ch=char.Parse(a);
+
+            //if (ch =='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') 
+            //{
+            //    Console.WriteLine("Vowel");
+            // }
+            //else
+            //{
+            //    Console.WriteLine("NOt Vowel");
+            //}
+            switch (ch)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':Console.WriteLine("Vowel");break;
+                default: Console.WriteLine("Not Vowel");break;
+            }
+            goto xyz;
+
+        }
+    }
+}
