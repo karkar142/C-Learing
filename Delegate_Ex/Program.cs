643cb13ea6b15599f81c68e927943c33a8d8546e
Wai Zin Aung <waizinaung.mm142@gmail.com>
2024-10-10 15:55:19 +0800

    Delegate_Ex

---
+++ Delegate_Ex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Ex
+{
+    internal class Program
+    {
+        delegate int Exchange(int x);
+        static void Main(string[] args)
+        {
+            //Exchange e = UsdToMMK;
+            //Console.WriteLine(e(3));
+
+            Exchange e = new Exchange(UsdToMMK);
+            Console.WriteLine(e.Invoke(3));
+        }
+        static int UsdToMMK(int usd)
+        {
+            return usd * 3000;
+        }
+    }
+}
